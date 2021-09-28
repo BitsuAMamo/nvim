31cf409184b7c9ad764027ad721d34ec0b6fbb3c
Bitsu A. Mamo <54959510+BitsuAMamo@users.noreply.github.com>
2021-09-28 15:06:23 +0300

    Configured indentation and highlight for treesitter

---
+++ lua/config/nvim_treesitter.lua
@@ -0,0 +1,14 @@
+-- Highlight
+require'nvim-treesitter.configs'.setup{
+    highlight = {
+        enable=true,
+    }
+}
+
+-- Indentation
+require'nvim-treesitter.configs'.setup {
+  indent = {
+    enable = true
+  }
+}
+
