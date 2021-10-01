cd17b446b5679f93ced38639d2808a69c499979f
Bitsu A. Mamo <54959510+BitsuAMamo@users.noreply.github.com>
2021-10-01 16:54:37 +0300

    Added diagnostics

--- lua/config/nvim_lsp.lua
+++ lua/config/nvim_lsp.lua
@@ -14,3 +14,10 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd[[ do User LspAttachBuffers ]]
 end)
 
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+})
+
