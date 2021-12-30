0fbdcab119f0868e16fffcfc6afa212fdd16a5b3
Bitsu Mamo <bitsua.mamo@gmail.com>
2021-12-30 11:00:26 -0800

    Removed all files

--- lua/config/dev.lua
+++
@@ -1,2 +0,0 @@
-require("nvim-autopairs").setup{}
-require("project_nvim").setup{}
--- lua/config/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.telescope")
-require("config.nvim_lsp")
-require("config.nvim_treesitter")
-require("config.nvim_cmp")
-require("config.tab_status_line")
-require("config.dev")
