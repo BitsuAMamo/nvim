fc76306b7eb015d6fc1a54772667461401f325e3
Bitsu Mamo <bitsua.mamo@gmail.com>
2022-12-16 12:06:52 -0600

    Starting from scratch

--- lua/user/comment.lua
+++
@@ -1,7 +0,0 @@
-local status, comment = pcall(require, "Comment")
-if not status then
-  print("Comment Failed to Load")
-  return
-end
-
-comment.setup()
--- lua/user/lualine.lua
+++
@@ -1,8 +0,0 @@
-local status, lualine = pcall(require, "lualine")
-if not status then
-  print("Lualine Failed to Load")
-  return
-end
-
-
-lualine.setup()
